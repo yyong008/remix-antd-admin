79c0cc3f588f5b503f12cfb91e0b2b90a5f565eb
Magnesium- <yyong007@outlook.com>
2023-04-27 20:02:49 +0800

    fix: client only use root not single

--- app/routes/($lang).user.login.tsx
+++ app/routes/($lang).user.login.tsx
@@ -42,7 +42,6 @@ import loginStyleUrl from "~/styles/login.css";
 // auth
 import { auth, sessionStorage } from "~/utils/auth.server";
 
-
 export const meta: V2_MetaFunction = () => {
   return [
     {
@@ -102,137 +101,135 @@ const LoginPage: React.FC = () => {
   };
 
   return (
-    <ClientOnlyWrap>
-      <LoginContainer>
-        <LoginContent>
-          <LoginForm
-            contentStyle={{
-              minWidth: 280,
-              maxWidth: "75vw",
-            }}
-            logo={<img alt="logo" src="/logo.png" />}
-            title={t("title")}
-            subTitle={t("desc")}
-            initialValues={{
-              autoLogin: true,
-              username: "admin",
-              password: "123456",
-            }}
-            actions={[t("other-login"), <ActionIcons key="icons" />]}
-            onFinish={async (values: string) => {
-              await handleSubmit(values);
-            }}
-            submitter={{
-              searchConfig: {
-                submitText: t("submit"),
+    <LoginContainer>
+      <LoginContent>
+        <LoginForm
+          contentStyle={{
+            minWidth: 280,
+            maxWidth: "75vw",
+          }}
+          logo={<img alt="logo" src="/logo.png" />}
+          title={t("title")}
+          subTitle={t("desc")}
+          initialValues={{
+            autoLogin: true,
+            username: "admin",
+            password: "123456",
+          }}
+          actions={[t("other-login"), <ActionIcons key="icons" />]}
+          onFinish={async (values: string) => {
+            await handleSubmit(values);
+          }}
+          submitter={{
+            searchConfig: {
+              submitText: t("submit"),
+            },
+          }}
+        >
+          <Tabs
+            activeKey={type}
+            onChange={setType}
+            centered
+            items={[
+              {
+                key: "account",
+                label: t("account-password-login"),
               },
-            }}
-          >
-            <Tabs
-              activeKey={type}
-              onChange={setType}
-              centered
-              items={[
-                {
-                  key: "account",
-                  label: t("account-password-login"),
-                },
-                {
-                  key: "mobile",
-                  label: t("phone-number-login"),
-                },
-              ]}
-            />
-            {type === "account" && (
-              <>
-                <ProFormText
-                  name="username"
-                  fieldProps={{
-                    size: "large",
-                    prefix: <UserOutlined />,
-                  }}
-                  placeholder={t("user-placeholder") as string}
-                  rules={[
-                    {
-                      required: true,
-                      message: t("user-message")!,
-                    },
-                  ]}
-                />
-                <ProFormText.Password
-                  name="password"
-                  fieldProps={{
-                    size: "large",
-                    prefix: <LockOutlined />,
-                  }}
-                  placeholder={t("password-pladeholder") as string}
-                  rules={[
-                    {
-                      required: true,
-                      message: t("password-message") as string,
-                    },
-                  ]}
-                />
-              </>
-            )}
-            {type === "mobile" && (
-              <>
-                <ProFormText
-                  fieldProps={{
-                    size: "large",
-                    prefix: <MobileOutlined />,
-                  }}
-                  name="mobile"
-                  placeholder={t("phone-placeholder")!}
-                  rules={[
-                    {
-                      required: true,
-                      message: t("phone-message")!,
-                    },
-                    {
-                      pattern: /^1\d{10}$/,
-                      message: t("phone-format-message")!,
-                    },
-                  ]}
-                />
-                <ProFormCaptcha
-                  fieldProps={{
-                    size: "large",
-                    prefix: <LockOutlined />,
-                  }}
-                  captchaProps={{
-                    size: "large",
-                  }}
-                  placeholder={t("verification-code")!}
-                  captchaTextRender={(timing: string, count: number) => {
-                    if (timing) {
-                      return `${count} ${t("get-verification-code")}`;
-                    }
-                    return t("get-verification-code");
-                  }}
-                  name="captcha"
-                  rules={[
-                    {
-                      required: true,
-                      message: t("verification-code")!,
-                    },
-                  ]}
-                  onGetCaptcha={async () => {
-                    message.success(t("get-captcha")!);
-                  }}
-                />
-              </>
-            )}
-            <HDiv>
-              <ProFormCheckbox noStyle name="autoLogin">
-                {t("remeber")}
-              </ProFormCheckbox>
-            </HDiv>
-          </LoginForm>
-        </LoginContent>
-        <Footer />
-      </LoginContainer>
-    </ClientOnlyWrap>
+              {
+                key: "mobile",
+                label: t("phone-number-login"),
+              },
+            ]}
+          />
+          {type === "account" && (
+            <>
+              <ProFormText
+                name="username"
+                fieldProps={{
+                  size: "large",
+                  prefix: <UserOutlined />,
+                }}
+                placeholder={t("user-placeholder") as string}
+                rules={[
+                  {
+                    required: true,
+                    message: t("user-message")!,
+                  },
+                ]}
+              />
+              <ProFormText.Password
+                name="password"
+                fieldProps={{
+                  size: "large",
+                  prefix: <LockOutlined />,
+                }}
+                placeholder={t("password-pladeholder") as string}
+                rules={[
+                  {
+                    required: true,
+                    message: t("password-message") as string,
+                  },
+                ]}
+              />
+            </>
+          )}
+          {type === "mobile" && (
+            <>
+              <ProFormText
+                fieldProps={{
+                  size: "large",
+                  prefix: <MobileOutlined />,
+                }}
+                name="mobile"
+                placeholder={t("phone-placeholder")!}
+                rules={[
+                  {
+                    required: true,
+                    message: t("phone-message")!,
+                  },
+                  {
+                    pattern: /^1\d{10}$/,
+                    message: t("phone-format-message")!,
+                  },
+                ]}
+              />
+              <ProFormCaptcha
+                fieldProps={{
+                  size: "large",
+                  prefix: <LockOutlined />,
+                }}
+                captchaProps={{
+                  size: "large",
+                }}
+                placeholder={t("verification-code")!}
+                captchaTextRender={(timing: string, count: number) => {
+                  if (timing) {
+                    return `${count} ${t("get-verification-code")}`;
+                  }
+                  return t("get-verification-code");
+                }}
+                name="captcha"
+                rules={[
+                  {
+                    required: true,
+                    message: t("verification-code")!,
+                  },
+                ]}
+                onGetCaptcha={async () => {
+                  message.success(t("get-captcha")!);
+                }}
+              />
+            </>
+          )}
+          <HDiv>
+            <ProFormCheckbox noStyle name="autoLogin">
+              {t("remeber")}
+            </ProFormCheckbox>
+          </HDiv>
+        </LoginForm>
+      </LoginContent>
+      <Footer />
+    </LoginContainer>
   );
 };
 
