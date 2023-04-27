d77a7c2a484acbdd246bb6f299f4c42b6203dee0
Magnesium- <yyong007@outlook.com>
2023-04-27 20:06:15 +0800

    fix: _index.tsx login put loader function

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,15 +1,6 @@
 // cores
+import { redirect } from "@remix-run/node";
 import { useNavigate } from "@remix-run/react";
 import { useEffect } from "react";
 
-const IndexPage = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    navigate("/zh/user/login");
-  }, [navigate]);
-
-  return null;
-};
-
-export default IndexPage;
+export const loader = () => redirect('/zh/user/login')
