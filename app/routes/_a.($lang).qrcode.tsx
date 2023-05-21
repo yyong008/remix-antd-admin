4275a6e2d41ecf17b4138b96591336f09b123262
Magnesium- <yyong007@outlook.com>
2023-05-21 15:04:01 +0800

    feat: qrcode list show qrcode

--- app/routes/_a.($lang).qrcode.tsx
+++ app/routes/_a.($lang).qrcode.tsx
@@ -1,10 +1,32 @@
 import { List, Space } from "antd";
+import { useEffect, useRef } from "react";
 import QrCodeList from "~/components/QrCodeList";
+import QRCode from "qrcode";
+
+const ReactQrCode = ({ url }: any) => {
+  const cRef = useRef<any>();
+
+  const drawQrCode = async () => {
+    let canvas = document.createElement("canvas");
+
+    await QRCode.toCanvas(canvas, url, { width: 132}).catch((err) => {
+      console.log("qr err", err);
+    });
+    cRef.current.appendChild(canvas)
+  };
+
+  useEffect(() => {
+    drawQrCode();
+  }, []);
+  
+  return (
+    <div ref={cRef}></div>
+  );
+};
 
 export default function QrCodeRoute() {
   const list = [
     { name: "qrcode", url: "https://www.npmjs.com/package/qrcode" },
-    { name: "article", url: "https://juejin.cn/article" },
     {
       name: "remix-antd-admin",
       url: "https://github.com/yyong008/remix-antd-admin",
@@ -14,13 +36,23 @@ export default function QrCodeRoute() {
   return (
     <div className="qr-mount">
       <>
-        <Space direction="vertical">
+        <Space
+          direction="vertical"
+          style={{
+            width: "100%",
+          }}
+        >
           <QrCodeList list={list} />
           <List
             bordered
             dataSource={list}
             renderItem={(item) => {
-              return <List.Item>{item.url}</List.Item>;
+              return (
+                <List.Item>
+                  <List.Item.Meta title={item.name} description={item.url} />
+                  <ReactQrCode url={item.url} />
+                </List.Item>
+              );
             }}
           />
         </Space>
