004ac8f99251ac313a8b736a284a2795920bbbb4
Magnesium- <yyong007@outlook.com>
2024-04-18 19:33:45 +0800

    ✅ ShowType test pass

---
+++ app/__tests__/components/common/ShowType.spec.tsx
@@ -0,0 +1,49 @@
+import { expect, it } from "vitest";
+import "@testing-library/jest-dom/vitest";
+import { render, screen } from "@testing-library/react";
+
+// component
+import ShowType from "~/components/common/ShowType";
+
+describe("test ShowType component", () => {
+  beforeEach(() => {});
+
+  it("test ShowType Component isShow null", () => {
+    const testid = "testid";
+    const WrapRole = () => (
+      <div data-testid={testid}>
+        <ShowType isShow={null} />
+      </div>
+    );
+    render(<WrapRole />);
+    const showType = screen.getByTestId(testid);
+    expect(showType).toBeInTheDocument();
+    expect(showType.innerHTML).toContain("-");
+  });
+
+  it("test ShowType Component isShow 0", () => {
+    const testid = "testid";
+    const WrapRole = () => (
+      <div data-testid={testid}>
+        <ShowType isShow={0} />
+      </div>
+    );
+    render(<WrapRole />);
+    const showType = screen.getByTestId(testid);
+    expect(showType).toBeInTheDocument();
+    expect(showType.innerHTML).toContain("否");
+  });
+
+  it("test ShowType Component isShow 1", () => {
+    const testid = "testid";
+    const WrapRole = () => (
+      <div data-testid={testid}>
+        <ShowType isShow={1} />
+      </div>
+    );
+    render(<WrapRole />);
+    const showType = screen.getByTestId(testid);
+    expect(showType).toBeInTheDocument();
+    expect(showType.innerHTML).toContain("是");
+  });
+});
