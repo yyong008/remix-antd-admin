0478b0413400e76b968598d0ac447b3f413dd956
Magnesium- <yyong007@outlook.com>
2024-04-18 13:05:51 +0800

    🔥 LinkType test

---
+++ app/__tests__/components/common/LinkType.spec.tsx
@@ -0,0 +1,46 @@
+import { expect, it } from "vitest";
+import "@testing-library/jest-dom/vitest";
+import { render, screen } from "@testing-library/react";
+
+// component
+import LinkType from "~/components/common/LinkType";
+
+describe("test Footer component", () => {
+  beforeEach(() => {});
+
+  it("test LinkType Component isLink null", () => {
+    const WrapRole = () => (
+      <div data-testid="link-type">
+        <LinkType isLink={null} />
+      </div>
+    );
+    render(<WrapRole />);
+    const linkType = screen.getByTestId("link-type");
+    expect(linkType).toBeInTheDocument();
+    expect(linkType.innerHTML).toContain("-");
+  });
+
+  it("test CacheType Component isLink 0", () => {
+    const WrapRole = () => (
+      <div data-testid="link-type">
+        <LinkType isLink={0} />
+      </div>
+    );
+    render(<WrapRole />);
+    const linkType = screen.getByTestId("link-type");
+    expect(linkType).toBeInTheDocument();
+    expect(linkType.innerHTML).toContain("否");
+  });
+
+  it("test CacheType Component isLink 1", () => {
+    const WrapRole = () => (
+      <div data-testid="link-type">
+        <LinkType isLink={1} />
+      </div>
+    );
+    render(<WrapRole />);
+    const linkType = screen.getByTestId("link-type");
+    expect(linkType).toBeInTheDocument();
+    expect(linkType.innerHTML).toContain("是");
+  });
+});
