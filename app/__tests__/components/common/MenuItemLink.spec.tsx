cfde60e7edc134777da6c9e6eb1d0f157429a230
Magnesium- <yyong007@outlook.com>
2024-04-18 14:32:54 +0800

    ✅ MenuItemLink pass

---
+++ app/__tests__/components/common/MenuItemLink.spec.tsx
@@ -0,0 +1,58 @@
+// types
+import { useState, type ComponentType } from "react";
+
+// libs
+import { expect, it } from "vitest";
+import { createRemixStub } from "@remix-run/testing";
+import { render, screen } from "@testing-library/react";
+
+import "@testing-library/jest-dom/vitest";
+
+// component
+import MenuItemLink from "~/components/common/MenuItemLink";
+
+describe("test MenuItemLink component", () => {
+  let Comp: ComponentType<{}>;
+  const testid = "menu-item-link";
+  beforeEach(() => {});
+
+  it("test MenuItemLink dom", async () => {
+    // eslint-disable-next-line react/display-name
+    Comp = () => {
+      const [pathName, setPathName] = useState("/about");
+      return (
+        // eslint-disable-next-line jsx-a11y/aria-role
+        <div data-testid={testid}>
+          <MenuItemLink
+            path={pathName}
+            dom={<>test dom</>}
+            setPathname={() => {
+              setPathName("/index");
+            }}
+          />
+        </div>
+      );
+    };
+    const RemixStub = createRemixStub([
+      {
+        path: "/",
+        Component: Comp,
+      },
+    ]);
+    render(<RemixStub initialEntries={["/"]} />);
+    const menuItemLink = await screen.getByTestId(testid);
+    expect(menuItemLink.innerHTML).toContain("test dom");
+    expect(menuItemLink.children[0].getAttribute("href")).toBe("/about");
+    expect(menuItemLink).toMatchInlineSnapshot(`
+      <div
+        data-testid="menu-item-link"
+      >
+        <a
+          href="/about"
+        >
+          test dom
+        </a>
+      </div>
+    `);
+  });
+});
