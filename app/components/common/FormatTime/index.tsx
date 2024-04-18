8286a2ade03ffeeaaeed982dee59009df2b99553
Magnesium- <yyong007@outlook.com>
2024-04-18 12:48:06 +0800

    🏷️ FormatTime Component add Types

--- app/components/common/FormatTime/index.tsx
+++ app/components/common/FormatTime/index.tsx
@@ -1,5 +1,13 @@
+// types
+import type dayjs from "dayjs";
+
 import { formatDate } from "~/utils/utils";
 
-export default function FormatTime({ timeStr }: any) {
-  return <>{timeStr ? formatDate(timeStr) : "-"}</>;
+type FormatTimeProps = {
+  timeStr: string | number | Date | dayjs.Dayjs | null | undefined;
+  format?: string;
+};
+
+export default function FormatTime({ timeStr, format }: FormatTimeProps) {
+  return <>{timeStr ? formatDate(timeStr, format) : "-"}</>;
 }
