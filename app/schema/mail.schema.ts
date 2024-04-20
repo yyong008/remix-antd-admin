c5936b5e736f905cf90d077c721e4231ca4bd29b
Magnesium- <yyong007@outlook.com>
2024-04-20 22:31:19 +0800

    🦺 add mail schema

--- app/schema/mail.schema.ts
+++ app/schema/mail.schema.ts
@@ -2,28 +2,36 @@ import { z } from "zod";
 
 // 发送邮件
 export const PostMailSchema = z.object({
-  name: z.string(),
-  // TODO: 补齐
+  name: z.string().optional(),
+  title: z.string().optional(),
+  host: z.string().optional(),
+  port: z.string().optional(),
+  user: z.string().optional(),
+  pass: z.string().optional(),
+  from: z.string().optional(),
+  to: z.string().optional(),
+  subject: z.string().optional(),
+  content: z.string().optional(),
+  html: z.string().optional(),
+  text: z.string().optional(),
 });
 
 // 邮件模板
-export const CreateMailTemplateSchema = z.object({
-  name: z.string(),
-  // TODO: 补齐
+export const CreateMailTemplateSchema = PostMailSchema.extend({});
+
+export const UpdateMailTemplateSchema = PostMailSchema.extend({
+  id: z.number(),
 });
-export const UpdateMailTemplateSchema = z.object({
-  name: z.string(),
-  // TODO: 补齐
+
+export const FindMailTemplateByIdSchema = z.object({
+  id: z.number(),
 });
-export const findMailTemplateByIdSchema = z.object({
-  name: z.string(),
-  // TODO: 补齐
+
+export const FindMailTemplateByPageSchema = z.object({
+  page: z.number().optional(),
+  pageSize: z.number().optional(),
 });
-export const findMailTemplateByPageSchema = z.object({
-  name: z.string(),
-  // TODO: 补齐
-});
+
 export const DeleteMailTemplateByIdsSchema = z.object({
-  name: z.string(),
-  // TODO: 补齐
+  ids: z.array(z.number()),
 });
