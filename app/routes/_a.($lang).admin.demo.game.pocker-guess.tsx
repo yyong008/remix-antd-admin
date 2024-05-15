fd37780f3f07c957e1bfd4c26dca0f93b3735229
Magnesium- <yyong007@outlook.com>
2024-05-15 12:50:10 +0800

    ✨ features impls remix game

--- app/routes/_a.($lang).admin.demo.game.pocker-guess.tsx
+++ app/routes/_a.($lang).admin.demo.game.pocker-guess.tsx
@@ -1,47 +1,6 @@
-import type { MetaFunction } from "@remix-run/react";
+import { Component } from "~/features/admin/demo/game/pocker-guess/Component";
+export { meta } from "~/features/admin/demo/game/pocker-guess/meta";
 
-// remix
-import { useNavigate, useParams } from "@remix-run/react";
-
-// styles
-import "~/styles/game/pocker-card.css";
-import "animate.css";
-import { PageContainer, ProCard } from "@ant-design/pro-components";
-
-// remix:meta
-export const meta: MetaFunction = () => {
-  return [{ title: "game-pocker-guess" }];
-};
-
-const PockerCard = (props: { content: string; addr: string }) => {
-  const navigate = useNavigate();
-  const { lang } = useParams();
-
-  const handleJump = (k: string) => {
-    navigate(`/${lang}/game/pockercontent/${k}`);
-  };
-  return (
-    <div
-      className="card"
-      onClick={() => {
-        handleJump(props.addr);
-      }}
-    >
-      {props.content}
-    </div>
-  );
-};
-export default function GamePockerGuess() {
-  return (
-    <PageContainer>
-      <ProCard>
-        <div className="card-container bg-orange-400">
-          <PockerCard content="♠" addr="hei" />
-          <PockerCard content="♥" addr="hong" />
-          <PockerCard content="♣" addr="mei" />
-          <PockerCard content="♦" addr="zhuan" />
-        </div>
-      </ProCard>
-    </PageContainer>
-  );
+export default function Route() {
+  return <Component />;
 }
--- app/routes/_a.($lang).admin.demo.game.rl.tsx
+++ app/routes/_a.($lang).admin.demo.game.rl.tsx
@@ -1,64 +1,6 @@
-// types
-import { ProCard } from "@ant-design/pro-components";
-import { PageContainer } from "@ant-design/pro-layout";
-import type { MetaFunction } from "@remix-run/node";
+import { Component } from "~/features/admin/demo/game/rl/Component";
+export { meta } from "~/features/admin/demo/game/rl/meta";
 
-// react
-import { useRef, useEffect } from "react";
-
-// rxjs
-import { fromEvent, merge } from "rxjs";
-import { scan, filter, map } from "rxjs/operators";
-
-// remix:meta
-export const meta: MetaFunction = () => {
-  return [{ title: "game-rl" }];
-};
-
-const step = 10;
-
-export default function Game() {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-
-  useEffect(() => {
-    const canvas = canvasRef.current! as HTMLCanvasElement;
-    const context = canvas.getContext("2d") as any;
-
-    const leftKeyStream = fromEvent(document, "keydown").pipe(
-      filter((event: any) => event.key === "ArrowLeft"),
-      map(() => -step),
-    );
-
-    const rightKeyStream = fromEvent(document, "keydown").pipe(
-      filter((event: any) => event.key === "ArrowRight"),
-      map(() => step),
-    );
-
-    const moveStream = merge(leftKeyStream, rightKeyStream).pipe(
-      scan((x, dx) => Math.max(0, Math.min(400 - 50, x + dx)), 0),
-    );
-
-    const subscription = moveStream.subscribe((x) => {
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      context.fillRect(x, 90, 50, 50);
-    });
-
-    return () => subscription.unsubscribe();
-  }, []);
-
-  return (
-    <PageContainer>
-      <ProCard>
-        <div className="flex flex-col items-center justify-center h-[70vh] bg-red-400">
-          <h1>左右移动方块游戏 (RxJS)</h1>
-          <canvas
-            ref={canvasRef}
-            width={400}
-            height={200}
-            style={{ border: "1px solid black" }}
-          />
-        </div>
-      </ProCard>
-    </PageContainer>
-  );
+export default function Route() {
+  return <Component />;
 }
