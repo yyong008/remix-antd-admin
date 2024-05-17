41d3ec9759633b7c14e986bd95fe5718fba76d95
Magnesium- <yyong007@outlook.com>
2024-05-17 08:41:20 +0800

    ✨ features impls register version

---
+++ app/features/welcome/Component.tsx
@@ -0,0 +1,15 @@
+import { Card } from "antd";
+import { PageContainer } from "@ant-design/pro-components";
+import { useTranslation } from "react-i18next";
+
+export function Component() {
+  const { t } = useTranslation();
+
+  return (
+    <PageContainer>
+      <Card>
+        <div>{t("welcome")}</div>
+      </Card>
+    </PageContainer>
+  );
+}
--- app/routes/($lang).admin.welcome.tsx
+++ app/routes/($lang).admin.welcome.tsx
@@ -1,30 +1,6 @@
-// types
-import type { MetaFunction } from "@remix-run/node";
+import { Component } from "~/features/welcome/Component";
+export { meta } from "~/features/welcome/meta";
 
-// components
-import { Card } from "antd";
-import { PageContainer } from "@ant-design/pro-components";
-
-// i18n
-import { useTranslation } from "react-i18next";
-
-// remix:meta
-export const meta: MetaFunction = () => {
-  return [
-    {
-      title: "欢迎~",
-    },
-  ];
-};
-
-export default function WelcomePage() {
-  const { t } = useTranslation();
-
-  return (
-    <PageContainer>
-      <Card>
-        <div>{t("welcome")}</div>
-      </Card>
-    </PageContainer>
-  );
+export default function Route() {
+  return <Component />;
 }
