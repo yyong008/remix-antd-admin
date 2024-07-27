7a92e8ab751fa6f6f0a713474e8816d9486b9317
Magnesium- <yyong007@outlook.com>
2024-07-27 19:11:56 +0800

    fe: use getNews$

--- app/modules-client/news/loaders/index.ts
+++ app/modules-client/news/loaders/index.ts
@@ -1,7 +1,8 @@
 import type * as tn from "@remix-run/node";
 
-import { getNews } from "~/dals/news/news";
+import { getNews$ } from "@/dals/news/news";
+import { lastValueFrom } from "rxjs";
 
 export async function query(args: tn.LoaderFunctionArgs) {
-  return await getNews();
+  return await lastValueFrom(getNews$());
 }
