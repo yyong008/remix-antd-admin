eb06127ac8ef7ecc05c78e9fdfb16be513f6d6e2
Magnesium- <yyong007@outlook.com>
2024-12-15 18:46:21 +0800

    ✨ use antd app to handle antd message context

--- app/modules/layout/_a/route.tsx
+++ app/modules/layout/_a/route.tsx
@@ -12,6 +12,7 @@ import { Outlet, useParams } from "@remix-run/react";
 import { ProLayout, WaterMark } from "@ant-design/pro-components";
 import { memo, useContext, useMemo, useState } from "react";
 
+import { App as AntdApp } from "antd";
 import { SettingContext } from "@/context";
 import { prolayoutConfig } from "@/config/prolayout";
 import { useGetUserInfoQuery } from "@/apis-client/common/user-info";
@@ -39,53 +40,55 @@ function AdminLayout() {
   );
 
   return (
-    <WaterMark content="Remix Antd Admin">
-      <ProLayout
-        location={{
-          pathname,
-        }}
-        route={route}
-        token={token}
-        loading={isLoading}
-        {...value.theme}
-        logo={prolayoutConfig.logo}
-        menu={prolayoutConfig.menu}
-        style={resetStyles}
-        title={prolayoutConfig.title}
-        ErrorBoundary={false}
-        pageTitleRender={false}
-        contentStyle={resetStyles}
-        layout={prolayoutConfig.layout as any}
-        footerRender={() => <Footer />}
-        suppressSiderWhenMenuEmpty={true}
-        menuFooterRender={MenuFooterRender}
-        actionsRender={createActionRenderWrap({ value })}
-        avatarProps={{
-          src: userInfo?.avatar || prolayoutConfig.avatar.src,
-          size: prolayoutConfig.avatar.size as any,
-          title: userInfo?.name,
-          render: (_, dom) => {
-            return <AvatarDropDown dom={dom} />;
-          },
-        }}
-        menuItemRender={(item, dom) => {
-          if (item.isLink) {
-            return <MenuItemOutLink path={item.path!} dom={dom} />;
-          }
+    <AntdApp>
+      <WaterMark content="Remix Antd Admin">
+        <ProLayout
+          location={{
+            pathname,
+          }}
+          route={route}
+          token={token}
+          loading={isLoading}
+          {...value.theme}
+          logo={prolayoutConfig.logo}
+          menu={prolayoutConfig.menu}
+          style={resetStyles}
+          title={prolayoutConfig.title}
+          ErrorBoundary={false}
+          pageTitleRender={false}
+          contentStyle={resetStyles}
+          layout={prolayoutConfig.layout as any}
+          footerRender={() => <Footer />}
+          suppressSiderWhenMenuEmpty={true}
+          menuFooterRender={MenuFooterRender}
+          actionsRender={createActionRenderWrap({ value })}
+          avatarProps={{
+            src: userInfo?.avatar || prolayoutConfig.avatar.src,
+            size: prolayoutConfig.avatar.size as any,
+            title: userInfo?.name,
+            render: (_, dom) => {
+              return <AvatarDropDown dom={dom} />;
+            },
+          }}
+          menuItemRender={(item, dom) => {
+            if (item.isLink) {
+              return <MenuItemOutLink path={item.path!} dom={dom} />;
+            }
 
-          return (
-            <MenuItemLink
-              path={item.path!}
-              dom={dom}
-              setPathname={setPathname}
-            />
-          );
-        }}
-      >
-        <Outlet />
-        <SettingDrawerWrap theme={value.theme} setTheme={value.setTheme} />
-      </ProLayout>
-    </WaterMark>
+            return (
+              <MenuItemLink
+                path={item.path!}
+                dom={dom}
+                setPathname={setPathname}
+              />
+            );
+          }}
+        >
+          <Outlet />
+          <SettingDrawerWrap theme={value.theme} setTheme={value.setTheme} />
+        </ProLayout>
+      </WaterMark>
+    </AntdApp>
   );
 }
 
