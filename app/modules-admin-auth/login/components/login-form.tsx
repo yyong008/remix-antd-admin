ac45f38196d73d9f43c3bc933b2d35049fa1e5c1
Magnesium- <yyong007@outlook.com>
2024-12-16 23:16:17 +0800

    ✨ login component advance

--- app/modules-admin-auth/login/components/login-form.tsx
+++ app/modules-admin-auth/login/components/login-form.tsx
@@ -3,23 +3,47 @@ import * as clientUtils from "@/utils/client";
 import { ConfigProvider, message } from "antd";
 import { Link, useNavigate, useParams } from "@remix-run/react";
 import { LoginForm, ProConfigProvider } from "@ant-design/pro-components";
-import {
-  setLocalStorageRefreshToken,
-  setLocalStorageToken,
-} from "@/libs/localstorage";
 
 import { ActionIcons } from "@/components/user-login";
 import { SettingContext } from "@/context";
 import { defaultLang } from "@/config/lang";
+import { simpleStorage } from "@/libs/localstorage";
 import { useContext } from "react";
 import { useLoginMutation } from "@/apis-client/auth";
 import { useNProgress } from "@/hooks";
 import { useTranslation } from "react-i18next";
 
+function LogoImg() {
+  return <img alt="logo" src="/logo.png" style={{ borderRadius: "10px" }} />;
+}
+
+function ActionOther({ t, lang }: any) {
+  return (
+    <div
+      className="flex justify-between items-centermt-[20px] text-black"
+      key="login-other"
+    >
+      <data className="flex items-center">
+        <div>{t("login-register.other-login")}</div>
+        <ActionIcons key="icons" />
+      </data>
+
+      <div className="flex justify-between items-center">
+        <Link to={`/${lang}/admin/register`}>register</Link>
+      </div>
+    </div>
+  );
+}
+
 export function LoginFormWrap({ children }: any) {
   useNProgress();
+  const initialValues = {
+    autoLogin: true,
+    username: "super admin",
+    password: "123456",
+  };
   const navigate = useNavigate();
-  const [login, loginOther] = useLoginMutation();
+  const [login, { isLoading }] = useLoginMutation();
   const value = useContext(SettingContext);
 
   const { t } = useTranslation();
@@ -32,8 +56,9 @@ export function LoginFormWrap({ children }: any) {
     };
     const result: any = await login(data);
     if (result.data.code === 0 && result.data.data.token?.length > 0) {
-      setLocalStorageToken(result.data.data.token);
-      setLocalStorageRefreshToken(result.data.data.refresh_token);
+      const { token, refresh_token } = result.data.data;
+      simpleStorage.setToken(token);
+      simpleStorage.setRefreshToken(refresh_token);
       message.success(result.data.message);
       navigate(`/${lang}/admin/dashboard`, { replace: true });
     } else {
@@ -49,39 +74,15 @@ export function LoginFormWrap({ children }: any) {
           token: value.theme,
         }}
       >
-        <div className="flex flex-col h-[100vh]  ">
+        <div className="flex flex-col h-[100vh]">
           <LoginForm
             className="flex-1 text-slate-950"
-            loading={loginOther.isLoading}
-            logo={
-              <img
-                alt="logo"
-                src="/logo.png"
-                style={{ borderRadius: "10px" }}
-              />
-            }
+            loading={isLoading}
+            logo={<LogoImg />}
             title={t("login-register.title")}
             subTitle={t("login-register.desc")}
-            initialValues={{
-              autoLogin: true,
-              username: "super admin",
-              password: "123456",
-            }}
-            actions={[
-              <div
-                className="flex justify-between items-centermt-[20px] text-black"
-                key="login-other"
-              >
-                <data className="flex items-center">
-                  <div>{t("login-register.other-login")}</div>
-                  <ActionIcons key="icons" />
-                </data>
-
-                <div className="flex justify-between items-center">
-                  <Link to={`/${lang}/admin/register`}>register</Link>
-                </div>
-              </div>,
-            ]}
+            initialValues={initialValues}
+            actions={[<ActionOther t={t} lang={lang} key="login-other" />]}
             onFinish={async (values: string) => {
               await handleSubmit(values);
             }}
