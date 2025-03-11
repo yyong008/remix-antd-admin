e56fcac3cc381c25022f4354cbecf9fcacdc7316
magnesium <yyong007@outlook.com>
2025-03-12 06:19:51 +0800

    ✨ Improve dev logger to log HTTP method and request body

--- app/apis.server/hono/middleware/dev.logger.ts
+++ app/apis.server/hono/middleware/dev.logger.ts
@@ -3,7 +3,15 @@ import { Context } from "hono";
 export function devLogger() {
   return async (c: Context, next: Function) => {
     if (process.env.NODE_ENV === "development") {
-      console.log("[request api url]: ", c.req.url);
+      console.log("[request api url]: ", c.req.method, c.req.url);
+      if (
+        c.req.method === "POST" ||
+        c.req.method === "PUT" ||
+        c.req.method === "DELETE"
+      ) {
+        const body = await c.req.json();
+        console.log("[request api json body]: ", JSON.stringify(body, null, 2));
+      }
     }
     await next();
   };
