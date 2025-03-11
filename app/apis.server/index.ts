2589fc8ae966621ff33e288b0ff591d18b48bdd3
magnesium <yyong007@outlook.com>
2025-03-12 05:25:14 +0800

    🏗️ Restructure API routing and create separate app configuration file

--- app/apis.server/index.ts
+++ app/apis.server/index.ts
@@ -1,19 +1,6 @@
-import { Hono } from "hono";
 import { handle } from "./hono/adapter";
 
-import { adminRouter } from "./admin";
-import { authRouter } from "./auth";
-import { commonRouter } from "./common";
-
-const app = new Hono();
-
-// app.use(createOperateMiddleware);
-
-// 因为国际化 `:lang` 存在，同一个服务提供多个路由，api 可能被匹配为 lang，添加版本号区分即可
-app.route("/api/v1", commonRouter);
-app.route("/api/v1/auth", authRouter);
-app.route("/api/v1/admin", adminRouter);
+import { app } from "./app";
 
 export const loader = handle(app);
 export const action = handle(app);
-
