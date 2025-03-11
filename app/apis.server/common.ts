c1c348adb04fc46d942ae72aeddd00e9bd584983
magnesium <yyong007@outlook.com>
2025-03-12 05:18:55 +0800

    ✨ Enhance healthcheck endpoint with database connectivity verification and error handling

--- app/apis.server/common.ts
+++ app/apis.server/common.ts
@@ -1,9 +1,32 @@
-import { Hono } from "hono";
+import { Context, Hono } from "hono";
+import prisma from "~/libs/prisma";
 
 export const commonRouter = new Hono();
 
-commonRouter.get("/healthcheck", async () => {
-  //
+commonRouter.get("/healthcheck", async (c: Context) => {
+  try {
+    await prisma.user.count();
+    return c.json({
+      code: 0,
+      message: {
+        database: "ok",
+      },
+      data: {},
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      return c.json({
+        code: 500,
+        message: error.message,
+        data: {},
+      });
+    }
+    return c.json({
+      code: 500,
+      message: (error as any)?.message ?? "",
+      data: {},
+    });
+  }
 });
 
 commonRouter.post("/upload", async () => {
