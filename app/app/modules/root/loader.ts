3924f4acdbc115aee4c2fc1d6884085999d73e90
magnesium <yyong007@outlook.com>
2025-03-13 22:14:02 +0800

    ✨ Implement language handling in loader function to redirect users based on language parameter and validate supported languages, enhancing internationalization support.

--- app/app/modules/root/loader.ts
+++ app/app/modules/root/loader.ts
@@ -1,9 +1,27 @@
 import type * as tn from "react-router";
-
-import { rps } from "~/utils/server";
+import { data, redirect } from "react-router";
+import { langs, defaultLang } from "~/config/lang";
 
 export const loader = (args: tn.LoaderFunctionArgs) => {
-  return rps.rfj({
+  const { lang } = args.params;
+  const url = new URL(args.request.url);
+  if (!lang) {
+    if (
+      !url.pathname.startsWith("/public") &&
+      !url.pathname.startsWith("/api") &&
+      !url.pathname.startsWith("/images")
+    ) {
+      return redirect(`/${defaultLang}${url.pathname}`);
+    }
+  } else {
+    if (!langs.includes(lang)) {
+      throw data("Not Lang Found", {
+        status: 404,
+        statusText: "Not found",
+      });
+    }
+  }
+  return {
     lang: args.params.lang,
-  });
+  };
 };
