a998a1f91a1662a88f0114f869c4d269d8aa0281
magnesium <yyong007@outlook.com>
2026-01-30 21:50:07 +0800

    Remove tsconfigPaths plugin from vitest configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,7 @@
-import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [
-    tsconfigPaths(),
   ],
   test: {
     globals: true,
