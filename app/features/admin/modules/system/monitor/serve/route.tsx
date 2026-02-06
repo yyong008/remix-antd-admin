475f6771057e0fd413032c9a5588cda023e357ef
magnesium <yyong007@outlook.com>
2026-02-06 17:31:55 +0800

    refactor: simplify system monitoring route and remove unused components
    - Streamlined the Route component to display a static message instead of dynamic monitoring data.
    - Removed unused components (Cpu, Disk, Mem, OsRuntime) and utility functions to clean up the codebase.
    - Updated the PageContainer to reflect the new focus on the Cloudflare Worker platform.

--- app/features/admin/modules/system/monitor/serve/route.tsx
+++ app/features/admin/modules/system/monitor/serve/route.tsx
@@ -1,55 +1,10 @@
-import { Col, Row } from "antd";
-import { useEffect, useState } from "react";
-
-import { Cpu } from "./components/cpu";
-import { Disk } from "./components/disk";
-import { Mem } from "./components/mem";
-import { OsRuntime } from "./components/os-runtime";
 import { PageContainer } from "@ant-design/pro-components";
-import { useMonitorServeInfo } from "~/api-client/queries/system-monitor-serve";
+
 
 export function Route() {
-	const [data, setData] = useState({
-		nodeRuntime: {},
-		osRuntime: {},
-		diskInfo: {},
-		memInfo: {},
-		cupInfo: {},
-		currentLoadInfo: {},
-	});
-
-	const { data: _data, isLoading } = useMonitorServeInfo();
-	useEffect(() => {
-		if (_data?.code === 0 && _data.data) {
-			setData(_data.data);
-		}
-	}, [_data, isLoading]);
-
-	const {
-		nodeRuntime,
-		osRuntime,
-		diskInfo,
-		memInfo,
-		cupInfo,
-		currentLoadInfo,
-	} = data;
-
 	return (
-		<PageContainer loading={isLoading}>
-			<Row gutter={[16, 16]}>
-				<Col span={12}>
-					<OsRuntime data={{ nodeRuntime, osRuntime }} />
-				</Col>
-				<Col span={12}>
-					<Disk data={{ diskInfo }} />
-				</Col>
-				<Col span={12}>
-					<Cpu data={{ cupInfo, currentLoadInfo }} />
-				</Col>
-				<Col span={12}>
-					<Mem data={{ memInfo }} />
-				</Col>
-			</Row>
+		<PageContainer loading={false}>
+			View on Cloudflare Worker platform
 		</PageContainer>
 	);
 }
--- app/features/admin/modules/system/monitor/serve/utils.ts
+++
@@ -1,7 +0,0 @@
-export const getGB = (val: number = 0) => {
-	return val ? (val / 1024 / 1024 / 1024).toFixed(1) : "0";
-};
-
-export const getPercent = (val: number = 0) => {
-	return val ? (val * 100).toFixed(2) + "%" : "0%";
-};
