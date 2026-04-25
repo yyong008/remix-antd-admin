469b536a0faed4914205ecb712ea4fa783b6be13
magnesium <yyong007@outlook.com>
2026-04-26 05:10:57 +0800

    refactor: restructure app handling to utilize Hono framework
    - Replaced the existing app structure with Hono for improved routing and request handling.
    - Moved API logic to a new hono/index.ts file, streamlining the fetch process.
    - Simplified the fetch method in app.ts to directly call the Hono app's fetch method.

--- workers/app.ts
+++ workers/app.ts
@@ -1,32 +1,5 @@
-import { app } from "../app/api/index";
-import { createRequestHandler, RouterContextProvider } from "react-router";
-import type { AppType } from "../app/api/index";
-
-declare const Env: { DB: D1Database; ctx: ExecutionContext };
-
-declare module "react-router" {
-  export interface AppLoadContext extends Record<string, any> {
-    cloudflare?: {
-      env: Env;
-      ctx: ExecutionContext;
-    };
-  }
-}
-
-const requestHandler = createRequestHandler(
-  () => import("virtual:react-router/server-build"),
-  import.meta.env.MODE,
-);
+import { app } from "./hono/index";
 
 export default {
-  async fetch(request, env, ctx) {
-    const { pathname } = new URL(request.url);
-    if (pathname.startsWith("/api")) {
-      const result = await app.fetch(request, env, ctx);
-      return result;
-    }
-    const loadContext = new RouterContextProvider();
-    Object.assign(loadContext, { cloudflare: { env, ctx } });
-    return await requestHandler(request, loadContext);
-  },
+  fetch: app.fetch,
 } satisfies ExportedHandler<Env>;
---
+++ workers/hono/index.ts
@@ -0,0 +1,21 @@
+import { Hono } from "hono";
+import { app as apiApp } from "../../app/api";
+import { createRequestHandler } from "react-router";
+
+declare module "react-router" {
+  export interface AppLoadContext extends Record<string, any> {
+    cloudflare?: {
+      env: Env;
+      ctx: ExecutionContext;
+    };
+  }
+}
+
+export const app = new Hono().route("/", apiApp).use("*", async (c) => {
+  const requestHandler = createRequestHandler(
+    () => import("virtual:react-router/server-build"),
+    import.meta.env.MODE,
+  );
+  return await requestHandler(c.req.raw);
+});
+export default app;
